3c5df802bbfab35ec7cc26f027af8776b0f89e6f
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-21 13:32:25 -0400

    more

--- lib/flatiron-lab-generator.rb
+++ lib/flatiron-lab-generator.rb
@@ -1,20 +1,34 @@
 require_relative '../config/environment.rb'
 
 module FlatironLabGenerator
-  class TemplateMaker
+  class TemplateMaker    
+    TEMPLATES = [
+      "procedural-ruby-lab-template",
+      "command-line-app-lab-template",
+      "SQL-lab-template",
+      "activerecord-template",
+      "rake-lab-template",
+      "erb-static-site-lab-template",
+      "rack-lab-template",
+      "sinatra-no-db-lab-template ",
+      "sinatra-lab-template"
+    ]
+
     attr_reader :template_type, :lab_name
+
     def initialize(template_type, lab_name)
       @template_type = template_type
       @lab_name = lab_name
     end
 
     def self.run(template_type, lab_name)
-      new(template_type, lab_name).create
+      new(template_type, lab_name).create(template_type)
     end
 
-    def create
-      #parses template, making new one in pwd and provided lab name
-
+    def parse(template_type)
+      #given the template type, make the file
+      #then
+      name_lab
       #calls methods
       git_init
       bundle_init
@@ -25,14 +39,22 @@ module FlatironLabGenerator
       end
     end
 
+    def name_lab
+      FileUtils.mv(template_type, lab_name)
+    end
+
     def git_init
       `git init`
     end
 
     def edit_readme
       File.open("README.md", 'a') do |f|
-        f << "\n# #{lab_name}"
+        f << "\n# #{formatted_name}"
       end
+    end
+
+    def formatted_name
+      lab_name.gsub('-', ' ').split.map(&:capitalize).join(' ')
     end
 
     def bundle_init
@@ -41,18 +63,13 @@ module FlatironLabGenerator
 
     #for procedural
     def change_filename
-      new_file_name
       FileUtils.cd('lib/') do
-        File.rename('file.rb', "#{new_file_name}.rb")
+        File.rename('file.rb', "#{lab_name}.rb")
       end
     end
 
-    def new_file_name
-      lab_name.gsub!(" ", "-")
-    end
-
     def change_spec_helper
-      new_rr = IO.read('spec/spec_helper.rb') % { :file_name => new_file_name }
+      new_rr = IO.read('spec/spec_helper.rb') % { file_name: lab_name }
       File.open('spec/spec_helper.rb', 'w') { |f| f.write(new_rr) }
     end
 
--- notes.md
+++ notes.md
@@ -1,11 +1,7 @@
 TODO
 
-1. basically mkdir with folders/files for each of the templates depending on the argument of the call. it accounts for gitignore and gemfile (no bundle command; the lab writer will do that after they determine their additional dependencies)
+* basically mkdir with folders/files for each of the templates depending on the argument of the call. it accounts for gitignore and gemfile (no bundle command; the lab writer will do that after they determine their additional dependencies)
 
-2. will need to incorporate bash scripts to do all of the generation?
+* will make the lab-template in the PWD of where the gem call is made
 
-3. You can specify the name of the lab so it creates the top level by that name.
-
-4. will make the lab-template in the PWD of where the gem call is made
-
-5. making a readme: parse the name of the lab and have it on the top
+* checks that the template names are valid and that name in the right format; if not through error.
