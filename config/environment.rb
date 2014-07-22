6c0dc3676fa08489e5310b062bce2a38ed6c9a4e
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-22 12:22:06 -0400

    moved filefinder

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@ require "flatiron-lab-generator/version.rb"
 require "fileutils"
 require "pry"
 
-require_relative "../lib/helpers/file-finder.rb"
+require_relative "../lib/file-finder.rb"
 require_relative "../lib/helpers/template-helper.rb"
 require_relative "../lib/helpers/gemfile-helper.rb"
