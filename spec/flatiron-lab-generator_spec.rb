4653f62cb751f64fd92944c36182cbe060161d85
arelenglish <arelenglish@gmail.com>
2015-01-15 17:52:21 -0500

    WIP writing test to make sure lab is actually generated

--- spec/flatiron-lab-generator_spec.rb
+++ spec/flatiron-lab-generator_spec.rb
@@ -7,4 +7,13 @@ describe FlatironLabGenerator::TemplateMaker do
       expect(lab_generator_object.git_init).to eq("Reinitialized existing Git repository in #{Dir.pwd}/.git/\n")
     end
   end
+
+  describe "::run" do
+    let(:lab_name) {"Test Lab"}
+    it "creates the lab with expected name and returns nil" do
+      expect(FlatironLabGenerator::TemplateMaker.run("fundamental-ruby",lab_name, "git repo")).to be_nil 
+      # Write another expectation to make sure the lab is actually created
+      FileUtils.rm_rf(lab_name)
+    end
+  end
 end
