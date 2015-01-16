d9cdb74b015fc2f756de85c8114d131fb6bb9ffc
arelenglish <arelenglish@gmail.com>
2015-01-16 15:34:27 -0500

    wrote a test for the ::run method

--- spec/flatiron-lab-generator_spec.rb
+++ spec/flatiron-lab-generator_spec.rb
@@ -4,16 +4,20 @@ describe FlatironLabGenerator::TemplateMaker do
   let(:lab_generator_object) {FlatironLabGenerator::TemplateMaker.new("fundamental-ruby","test_lab","git_repo")}
   describe "#git_init" do
     it "initializes a new local git repo" do
-      expect(lab_generator_object.git_init).to eq("Reinitialized existing Git repository in #{Dir.pwd}/.git/\n")
+      lab_generator_object.git_init
+      expect(system('git rev-parse')).to eq(true)
     end
   end
 
   describe "::run" do
-    let(:lab_name) {"Test Lab"}
+    let(:lab_name) {"test_lab"}
     it "creates the lab with expected name and returns nil" do
       expect(FlatironLabGenerator::TemplateMaker.run("fundamental-ruby",lab_name, "git repo")).to be_nil 
-      # Write another expectation to make sure the lab is actually created
+      expect(File.directory?(lab_name)).to eq(true) 
+      expect(`tree #{lab_name}`).to eq("test_lab\n├── Gemfile\n├── README.md\n├── lib\n│   └── test_lab.rb\n└── spec\n    ├── spec_helper.rb\n    └── test_lab_spec.rb\n\n2 directories, 5 files\n")
       FileUtils.rm_rf(lab_name)
     end
   end
+
+  
 end
