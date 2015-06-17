1a6984574ba1210149b083205ba164dd082b83af
Logan Hasson <logan.hasson@gmail.com>
2015-06-17 14:21:04 -0400

    Add template loader

---
+++ lib/learn_generate/template_loader.rb
@@ -0,0 +1,94 @@
+module LearnGenerate
+  class TemplateLoader
+    attr_reader   :internet, :local_path, :local_dir
+    attr_accessor :templates
+
+    BASE_TEMPLATES = [
+      'fundamental-ruby',
+      'command-line',
+      'sql',
+      'activerecord',
+      'rake',
+      'erb-static-site',
+      'rack',
+      'sinatra-classic',
+      'sinatra-mvc',
+      'js',
+      'front-end',
+      'kids'
+    ]
+
+    def initialize(internet)
+      @internet   = internet
+      @local_dir  = File.expand_path('~/.learn-generate')
+      @local_path = File.expand_path('~/.learn-generate/templates')
+
+      set_templates
+    end
+
+    def printable_list
+      templates.map.with_index do |template, index|
+        "#{index+1}. #{template}"
+      end.join("\n")
+    end
+
+    private
+
+    def set_templates
+      if internet
+        if has_local_copy?
+          get_and_update_templates
+        else
+          prep_local_copy
+          get_and_update_templates
+        end
+      elsif has_local_copy?
+        update_local_templates
+      else
+        set_base_templates
+      end
+    end
+
+    def prep_local_copy
+      create_local_dir
+      clone_templates_repo
+    end
+
+    def create_local_dir
+      FileUtils.mkdir_p(local_dir)
+    end
+
+    def clone_templates_repo
+      system("cd #{local_dir} && git clone https://github.com/learn-co-curriculum/learn-generate-templates.git templates")
+    end
+
+    def get_and_update_templates
+      get_latest_templates
+      update_local_templates
+    end
+
+    def get_latest_templates
+      system("cd #{local_path} && git pull --rebase")
+    end
+
+    def update_local_templates
+      self.templates = Dir.entries("#{local_path}/templates").reject {|f| f.start_with?('.') || !File.directory?("#{local_path}/templates/#{f}")}
+    end
+
+    def set_base_templates
+      self.templates = BASE_TEMPLATES
+    end
+
+    def has_local_copy?
+      has_templates_dir? && templates_dir_is_git_repo?
+    end
+
+    def has_templates_dir?
+      File.exists?(local_path) && File.directory?(local_path)
+    end
+
+    def templates_dir_is_git_repo?
+      File.exists?("#{local_path}/.git") && File.directory?("#{local_path}/.git")
+    end
+  end
+end
