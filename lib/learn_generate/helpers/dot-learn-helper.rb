f557be3a8b6ef6d95c1b48d0079dab71785307b4
Logan Hasson <logan.hasson@gmail.com>
2015-07-09 14:40:42 -0400

    Tweak .learn again

--- lib/learn_generate/helpers/dot-learn-helper.rb
+++ lib/learn_generate/helpers/dot-learn-helper.rb
@@ -3,7 +3,7 @@ module LearnGenerate
     module DotLearnHelper
       def build_dot_learn
         File.open('.learn', 'a') do |f|
-          f.write("tags:\n  - tag1\nlanguages:  - language1\nresources: 0")
+          f.write("tags:\n  - tag1\nlanguages:\n  - language1\nresources: 0")
         end
       end
     end
