71c19896019ee2b1de9491d1745f1277ad83275c
Logan Hasson <logan.hasson@gmail.com>
2015-12-03 11:51:29 -0500

    Better .learn generation

--- lib/learn_generate/helpers/dot-learn-helper.rb
+++ lib/learn_generate/helpers/dot-learn-helper.rb
@@ -3,7 +3,7 @@ module LearnGenerate
     module DotLearnHelper
       def build_dot_learn
         File.open('.learn', 'a') do |f|
-          f.write("tags:\n  - tag1\nlanguages:\n  - language1\nresources: 0")
+          f.write("tags:\n  - tag_1\n  - tag_2\nlanguages:\n  - language_1\n  - language_1\nresources: 0")
         end
       end
     end
